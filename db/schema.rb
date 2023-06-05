6f5e74c13b6639d4f26214fb12cea195b9e8504b
taiga39 <ishikawa.tigers@gmail.com>
2023-06-05 20:05:49 +0900

    パスワード暗号化の導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_105057) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_110118) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
