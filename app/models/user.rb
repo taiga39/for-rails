5b0c4f7af3e83216963908c532ae51e4ac07e82a
taiga39 <ishikawa.tigers@gmail.com>
2023-06-06 22:34:54 +0900

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,41 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
-  has_many :microposts, dependent: :destroy
+  attr_accessor :remember_token
 
-  default_scope -> { order(created_at: :desc) }
+  before_save { self.email = email.downcase }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = self.new_token
+    update_attribute(:remember_digest, self.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # 渡された文字列のハッシュ値を返す
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_130951) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_132245) do
   create_table "microposts", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_130951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
   add_foreign_key "microposts", "users"
